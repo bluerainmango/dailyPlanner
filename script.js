851fb617e52dc251d16141ca10f85e29dcabd027
Emily Yu <bluerainmango@gmail.com>
2020-01-20 01:00:35 -0800

    changed 12 noon -> 12pm

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-$('document').ready(function(){
+$("document").ready(function() {
+  var state = {
+    startHour: null, // variable to detect hour change
+    currentHour: null,
+    formatedTime: null,
+    setBeginingHour: 9, // set first time slot(default: 9(9am))
+    setEndingHour: 17 // set last time slot(default: 17(5pm))
+  };
 
-    var state = {
-        startHour : null, // variable to detect hour change
-        currentHour: null,
-        formatedTime: null,
-        setBeginingHour: 9, // set first time slot(default: 9(9am))
-        setEndingHour: 17 // set last time slot(default: 17(5pm))
-    }
-
-// Render time slots HTML
-function renderTimeSlot(){
-    
-    for( var i = state.setBeginingHour; i <= state.setEndingHour; i++ ){
-            var content = 
-                        `<div class="row">
-                            <div class="hour" data-hour=${i}>${ i <= 12 ? i+'AM' : i-12+'PM' }</div>
+  // Render time slots HTML
+  function renderTimeSlot() {
+    for (var i = state.setBeginingHour; i <= state.setEndingHour; i++) {
+      var content = `<div class="row">
+                        <div class="hour" data-hour=${i}>${i <= 12 ? i + "AM" : i - 12 + "PM"}</div>
                             <textarea class="content"></textarea>
                             <button class="btn">
                                 <svg class="icon icon-save-disk">
@@ -23,95 +20,93 @@ function renderTimeSlot(){
                                 </svg>
                                 <p>save</p>
                             </button>
-                        </div>`
+                        </div>`;
 
-            $('main').append(content);
+      $("main").append(content);
     }
 
-    $('[data-hour=12]').text('12 NOON');
-
-}
-// Import data from localStorage
-function fromLocalStorage(){
-    for( var i = state.setBeginingHour ; i <= state.setEndingHour ; i++ ){
-            var savedText = localStorage.getItem(JSON.stringify(i))
-            $(`[data-hour=${i}]`).siblings('textarea').val(savedText);
+    $("[data-hour=12]").text("12PM");
+  }
+  // Import data from localStorage
+  function fromLocalStorage() {
+    for (var i = state.setBeginingHour; i <= state.setEndingHour; i++) {
+      var savedText = localStorage.getItem(JSON.stringify(i));
+      $(`[data-hour=${i}]`)
+        .siblings("textarea")
+        .val(savedText);
     }
-}
-// Paint textarea field's bg
-function colorField(){
-
+  }
+  // Paint textarea field's bg
+  function colorField() {
     // 1. Remove previous 'current' color change
-    $('[class ~= "current"]').removeClass('current');
+    $('[class ~= "current"]').removeClass("current");
 
     // 2. Paint all fields accordingly
-    $('.hour').each(function(){
-       
-        var hourNum = $(this).attr('data-hour'); // 9 - 17
-        var hourToCompare = moment().hour(hourNum);
+    $(".hour").each(function() {
+      var hourNum = $(this).attr("data-hour"); // 9 - 17
+      var hourToCompare = moment().hour(hourNum);
 
-        // if current moment's hour === field's hour
-        if(moment().isSame(hourToCompare, 'hour')){
-            $(this).next().addClass('current');
-        }
-        // if current moment's hour is after field's hour
-        else if(moment().isAfter(hourToCompare, 'hour')){
-            $(this).next().addClass('past');
-        }
+      // if current moment's hour === field's hour
+      if (moment().isSame(hourToCompare, "hour")) {
+        $(this)
+          .next()
+          .addClass("current");
+      }
+      // if current moment's hour is after field's hour
+      else if (moment().isAfter(hourToCompare, "hour")) {
+        $(this)
+          .next()
+          .addClass("past");
+      }
+    });
+  }
+  // Timer( Update state data / Render header time / Detect hour change )
+  function timeSensor() {
+    setInterval(() => {
+      var momentObj = moment();
 
-    })
-}
-// Timer( Update state data / Render header time / Detect hour change )
-function timeSensor(){
+      // 1. State data update
+      state.currentHour = momentObj.hour();
+      state.formatedTime = momentObj.format("dddd, MMM Do YYYY, h:mm A");
 
-    setInterval(()=>{
+      // 2. Render header time DOM
+      $("#currentTime").text(state.formatedTime);
 
-        var momentObj = moment();
-
-        // 1. State data update
-        state.currentHour = momentObj.hour();
-        state.formatedTime = momentObj.format('dddd, MMM Do YYYY, h:mm A');
-
-        // 2. Render header time DOM
-        $('#currentTime').text(state.formatedTime);
-
-        // 3. Detect hour change and renew textarea field's bg color accordingly
-        if(state.startHour !== state.currentHour){ colorField() }
-
-    },500);
-    
-}  
-// Render 'Saved' pop message
-function renderSavedPop(clickedBtn){
-    $(clickedBtn).addClass('pop');
-    setTimeout( function(){ $(clickedBtn).removeClass('pop') },500 );
-}
-function init(){
-
+      // 3. Detect hour change and renew textarea field's bg color accordingly
+      if (state.startHour !== state.currentHour) {
+        colorField();
+      }
+    }, 500);
+  }
+  // Render 'Saved' pop message
+  function renderSavedPop(clickedBtn) {
+    $(clickedBtn).addClass("pop");
+    setTimeout(function() {
+      $(clickedBtn).removeClass("pop");
+    }, 500);
+  }
+  function init() {
     state.startHour = moment().hour();
     renderTimeSlot();
     fromLocalStorage();
     colorField();
     timeSensor();
+  }
 
-}
-
-
-// Button event 
-$('main').on('click', 'button, botton *', function(e){
-
+  // Button event
+  $("main").on("click", "button, botton *", function(e) {
     // Get hour & textarea data then save to localStorage
-        var clickedButton = $(e.target).closest('button');
+    var clickedButton = $(e.target).closest("button");
 
-        var hourForRecord = clickedButton.siblings('.hour').attr('data-hour'); 
-        var dataForRecord = clickedButton.siblings('.content').val().trim();
+    var hourForRecord = clickedButton.siblings(".hour").attr("data-hour");
+    var dataForRecord = clickedButton
+      .siblings(".content")
+      .val()
+      .trim();
 
-        localStorage.setItem(hourForRecord, dataForRecord);
-        renderSavedPop(clickedButton);
+    localStorage.setItem(hourForRecord, dataForRecord);
+    renderSavedPop(clickedButton);
+  });
 
-})
-
-init();
-
-
-})
+  init();
+});
